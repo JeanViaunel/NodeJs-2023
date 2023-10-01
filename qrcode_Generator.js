6ce67f5f19ee839538016e62dde4698ad1e97ba7
JeanViuanel <viocitovictor35@gmail.com>
2023-10-01 07:09:49 +0000

    google.com img

--- qrcode_Generator.js
+++ qrcode_Generator.js
@@ -1,5 +1,5 @@
 import qr from "qr-image";
 import fs from "fs";
-const data = "https://www.npmjs.com/package/qrcode";
+const data = "https://www.google.com/";
 const img = qr.image(data);
 img.pipe(fs.createWriteStream("img.png"));
